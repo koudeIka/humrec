fa2ffe0a1c23369ab15717d62ac7b6ada4cf09fc
koudeIka <patrick.peronny@laposte.net>
2014-08-22 21:43:31 +0200

    modif url rewriting

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 $html = new html();
 $html->doctype		= "html5";
 $html->title = "Hum.rec : Free and LOSS releases | ".HTML_TITLE;
-
+$html->base						= $_SERVER["HTTP_HOST"];
 // CSS COMMON PERSO
 $html->css[] 		= $INI['path']['js_library'].'fancybox/jquery.fancybox.css';
 $html->css[] 		= $INI['path']['css'].'general.css';
@@ -76,29 +76,33 @@ else
 echo "	</div>
 	<div id='entete_menu'>
 		<ul class='menu_langs'>
-			<li><a href='?lang=fr'";
-			if($_SESSION['lang'] == 'fr') { echo " class='selected'"; }
-			echo ">Français</a></li>
-			<li><a href='?lang=en'";
-			if($_SESSION['lang'] == 'en') { echo " class='selected'"; }
-			echo ">English</a></li>
+			<li><a href='?lang=fr' ".($_SESSION['lang'] == 'fr' ? " class='selected'" : "").">Français</a></li>
+			<li><a href='?lang=en' ".($_SESSION['lang'] == 'en' ? " class='selected'" : "").">English</a></li>
 		</ul>
 	</div>
 	<div id='centre'>
 		<div id='navigation'>
 			<ul class='navi_blur'>
-				<li><a href='".$INI['general']['url_public']."'";
-				if(CURRENT_PAGE == 'index.php') { echo  " class='active'"; }
-				echo ">News</a></li>
-				<li><a href='releases.php'";
-				if(CURRENT_PAGE == 'releases.php') { echo  " class='active'"; }
-				echo ">Releases</a></li>
-				<li><a href='artistes.php'";
-				if(CURRENT_PAGE == 'artistes.php') { echo  " class='active'"; }
-				echo ">Artistes</a></li>
-				<li><a href='about.php'";
-				if(CURRENT_PAGE == 'about.php') { echo  " class='active'"; }
-				echo ">A propos</a></li>
+				<li><a href='".$INI['general']['url_public']."' ".(CURRENT_PAGE == 'index.php' ? " class='active'" : "").">News</a></li>
+				<li><a href='releases.php' ".(CURRENT_PAGE == 'releases.php' ? " class='active'" : "").">Releases</a>
+					<ul id='releases_link' ".(CURRENT_PAGE == 'releases.php' ? " style='display: block;'" : "").">";
+					foreach(releases::getAll(true) as $rel_id)
+					{
+						$release = new releases($rel_id);
+						echo "<li><a href='/release::".$release->rel_url."' ".($_GET["rel_url"] == $release->rel_url ? " class='active'" : "").">".$release->rel_number."</a></li>";
+					}
+echo "				</ul>
+				</li>
+				<li><a href='artistes.php' ".(CURRENT_PAGE == 'artistes.php' ? " class='active'" : "").">Artistes</a>
+					<ul id='artistes_link' ".(CURRENT_PAGE == 'artistes.php' ? " style='display: block;'" : "").">";
+					foreach(artistes::getAll(true) as $art_id)
+					{
+						$artiste = new artistes($art_id);
+						echo "<li><a href='/artist::".$artiste->art_url."' ".($_GET["art_url"] == $artiste->art_url ? " class='active'" : "").">".$artiste->art_name."</a></li>";
+					}
+echo "				</ul>
+				</li>
+				<li><a href='about.php' ".(CURRENT_PAGE == 'about.php' ? " class='active'" : "").">A propos</a></li>
 			</ul>
 			<div id='newsletter'>
 			      <label>";
--- releases.php
+++ releases.php
@@ -1,4 +1,5 @@
 <?php
+
 define("HTML_TITLE", "Releases");
 require_once('application_top.php');
 include_once('header.php');
@@ -13,83 +14,158 @@ if($_SESSION['lang'] == 'fr')
 	$l10n_download = "Download";
 }
 
-
-if(!empty($_GET['release_name']))
+if($_GET["url"] == "rewrite")
 {
-	// print_r($_GET);
+	$rel_id = releases::getByUrl($_GET["rel_url"]);
+	if($rel_id !== false)
+	{
+		$release = new releases($rel_id);
+
+		$art_names = array();
+
+		foreach($release->art_ids as $art_id)
+		{
+			$artiste = new artistes($art_id);
+			$art_names[] = $artiste->art_name;
+		}
+		$pho_names = array();
+		foreach($release->pho_ids as $pho_id)
+		{
+			$photo = new photos($pho_id);
+			$pho_names[] = $photo->pho_name;
+		}
+		echo "<div class='release' data-rel_id='".$release->rel_id."'>";
+			echo "<a href='".$INI['photos_path']['real_size'].$pho_names[0]."' class='photo'><img src='".$INI['photos_path']['200x320'].$pho_names[0]."' alt='".$pho_names[0]."' class='release-cover' /></a>";
+			echo "<h2>".implode(" &amp; ", $art_names)." - ".$release->rel_title."</h2>";
+			echo "<h3>".$release->rel_number." | ".$release->rel_date."</h3>";
+
+			$rel_description = "rel_content_".$_SESSION['lang'];
+			echo "<p class='release-content'>".$release->$rel_description."</p>";
+
+			echo "<p class='release-content-footer'>".$l10n_description_footer."</p>";
+			echo "<div class='release-options'>";
+			if (!empty($release->rel_download))
+			{
+				echo "<div class='release-download'><a href='".$release->rel_download."' class='link_in_text' target='_blank'>".$l10n_download."</a></div>";
+				echo "<span class='separator'>|</span>";
+			}
+			if(!empty($release->rel_paypal_id))
+			{
+				echo "<form action='https://www.paypal.com/cgi-bin/webscr' method='post' class='paypalForm' target='_blank'>";
+				echo "<input type='hidden' name='cmd' value='_s-xclick' />";
+				echo "<input type='hidden' name='currency_code' value='".$release->rel_paypal_currency_code."'>";
+				echo "<input type='hidden' name='on0' value='".$release->rel_paypal_id."' />";
+				echo "<input type='hidden' name='os0' value='UE' />";
+				echo "<input type='hidden' name='encrypted' value=\"".$release->rel_paypal_encrypted."\" />";
+				echo "</form>";
+
+				echo "<div class='release-buy'>
+						<a href='https://www.paypal.com/cgi-bin/webscr' class='link_in_text' data-region='UE'><b>UE</b> ".$release->rel_paypal_priceue."&euro;</a>
+					</div>";
+				echo "<span class='separator'>|</span>";
+				echo "<div class='release-buy'>
+						<a href='https://www.paypal.com/cgi-bin/webscr' class='link_in_text' target='_blank' data-region='World'><b>WORLD</b> ".$release->rel_paypal_priceworld."&euro;</a>
+					</div>";
+			}
+			echo "</div>";
+			
+			echo "<audio preload='none' src='".$release->tracks[0]->tra_url."'></audio>";
+		
+			echo "<ul class='tracklist'>";
+			foreach($release->tracks as $track)
+			{
+				echo "	<li class='track-details' data-track_id='".$track->tra_id."'>
+					<a href='".$track->tra_name."' data-src='".$track->tra_url."'><span>".$track->tra_digit."</span> ".$track->tra_name."</a>
+				</li>";
+			}
+			echo "</ul>";
+
+			
+
+			echo "<div class='clear'>&nbsp;</div>";
+		echo "</div>";
+	}
+}
+else
+{
+
+
+	foreach (releases::getAll() as $rel_id)
+	{
+		$release = new releases($rel_id);
+
+		$art_names = array();
+
+		foreach($release->art_ids as $art_id)
+		{
+			$artiste = new artistes($art_id);
+			$art_names[] = $artiste->art_name;
+		}
+		$pho_names = array();
+		foreach($release->pho_ids as $pho_id)
+		{
+			$photo = new photos($pho_id);
+			$pho_names[] = $photo->pho_name;
+		}
+		echo "<div class='release' data-rel_id='".$release->rel_id."'>";
+			echo "<a href='".$INI['photos_path']['real_size'].$pho_names[0]."' class='photo'><img src='".$INI['photos_path']['200x320'].$pho_names[0]."' alt='".$pho_names[0]."' class='release-cover' /></a>";
+			echo "<h2>".implode(" &amp; ", $art_names)." - ".$release->rel_title."</h2>";
+			echo "<h3>".$release->rel_number." | ".$release->rel_date."</h3>";
+
+			$rel_description = "rel_content_".$_SESSION['lang'];
+			echo "<p class='release-content'>".$release->$rel_description."</p>";
+
+			echo "<p class='release-content-footer'>".$l10n_description_footer."</p>";
+			echo "<div class='release-options'>";
+			if (!empty($release->rel_download))
+			{
+				echo "<div class='release-download'><a href='".$release->rel_download."' class='link_in_text' target='_blank'>".$l10n_download."</a></div>";
+				echo "<span class='separator'>|</span>";
+			}
+			if(!empty($release->rel_paypal_id))
+			{
+				echo "<form action='https://www.paypal.com/cgi-bin/webscr' method='post' class='paypalForm' target='_blank'>";
+				echo "<input type='hidden' name='cmd' value='_s-xclick' />";
+				echo "<input type='hidden' name='currency_code' value='".$release->rel_paypal_currency_code."'>";
+				echo "<input type='hidden' name='on0' value='".$release->rel_paypal_id."' />";
+				echo "<input type='hidden' name='os0' value='UE' />";
+				echo "<input type='hidden' name='encrypted' value=\"".$release->rel_paypal_encrypted."\" />";
+				echo "</form>";
+
+				echo "<div class='release-buy'>
+						<a href='https://www.paypal.com/cgi-bin/webscr' class='link_in_text' data-region='UE'><b>UE</b> ".$release->rel_paypal_priceue."&euro;</a>
+					</div>";
+				echo "<span class='separator'>|</span>";
+				echo "<div class='release-buy'>
+						<a href='https://www.paypal.com/cgi-bin/webscr' class='link_in_text' target='_blank' data-region='World'><b>WORLD</b> ".$release->rel_paypal_priceworld."&euro;</a>
+					</div>";
+			}
+			echo "</div>";
+			
+			echo "<audio preload='none' src='".$release->tracks[0]->tra_url."'></audio>";
+		
+			echo "<ul class='tracklist'>";
+			foreach($release->tracks as $track)
+			{
+				echo "	<li class='track-details' data-track_id='".$track->tra_id."'>
+					<a href='".$track->tra_name."' data-src='".$track->tra_url."'><span>".$track->tra_digit."</span> ".$track->tra_name."</a>
+				</li>";
+			}
+			echo "</ul>";
+
+			
+
+			echo "<div class='clear'>&nbsp;</div>";
+		echo "</div>";
+	}
+
+
 }
 
 
 
 
 //echo "<h1>Releases</h1>";
-foreach (releases::getAll() as $rel_id)
-{
-	$release = new releases($rel_id);
 
-	$art_names = array();
-
-	foreach($release->art_ids as $art_id)
-	{
-		$artiste = new artistes($art_id);
-		$art_names[] = $artiste->art_name;
-	}
-	$pho_names = array();
-	foreach($release->pho_ids as $pho_id)
-	{
-		$photo = new photos($pho_id);
-		$pho_names[] = $photo->pho_name;
-	}
-	echo "<div class='release' data-rel_id='".$release->rel_id."'>";
-		echo "<a href='".$INI['photos_path']['real_size'].$pho_names[0]."' class='photo'><img src='".$INI['photos_path']['200x320'].$pho_names[0]."' alt='".$pho_names[0]."' class='release-cover' /></a>";
-		echo "<h2>".implode(" &amp; ", $art_names)." - ".$release->rel_title."</h2>";
-		echo "<h3>".$release->rel_number." | ".$release->rel_date."</h3>";
-
-		$rel_description = "rel_content_".$_SESSION['lang'];
-		echo "<p class='release-content'>".$release->$rel_description."</p>";
-
-		echo "<p class='release-content-footer'>".$l10n_description_footer."</p>";
-		echo "<div class='release-options'>";
-		if (!empty($release->rel_download))
-		{
-			echo "<div class='release-download'><a href='".$release->rel_download."' class='link_in_text' target='_blank'>".$l10n_download."</a></div>";
-			echo "<span class='separator'>|</span>";
-		}
-		if(!empty($release->rel_paypal_id))
-		{
-			echo "<form action='https://www.paypal.com/cgi-bin/webscr' method='post' class='paypalForm' target='_blank'>";
-			echo "<input type='hidden' name='cmd' value='_s-xclick' />";
-			echo "<input type='hidden' name='currency_code' value='".$release->rel_paypal_currency_code."'>";
-			echo "<input type='hidden' name='on0' value='".$release->rel_paypal_id."' />";
-			echo "<input type='hidden' name='os0' value='UE' />";
-			echo "<input type='hidden' name='encrypted' value=\"".$release->rel_paypal_encrypted."\" />";
-			echo "</form>";
-
-			echo "<div class='release-buy'>
-				    <a href='https://www.paypal.com/cgi-bin/webscr' class='link_in_text' data-region='UE'><b>UE</b> ".$release->rel_paypal_priceue."&euro;</a>
-			      </div>";
-			echo "<span class='separator'>|</span>";
-			echo "<div class='release-buy'>
-				    <a href='https://www.paypal.com/cgi-bin/webscr' class='link_in_text' target='_blank' data-region='World'><b>WORLD</b> ".$release->rel_paypal_priceworld."&euro;</a>
-			      </div>";
-		}
-		echo "</div>";
-		
-		echo "<audio preload='none' src='".$release->tracks[0]->tra_url."'></audio>";
-	
-		echo "<ul class='tracklist'>";
-		foreach($release->tracks as $track)
-		{
-			echo "	<li class='track-details' data-track_id='".$track->tra_id."'>
-				<a href='".$track->tra_name."' data-src='".$track->tra_url."'><span>".$track->tra_digit."</span> ".$track->tra_name."</a>
-			</li>";
-		}
-		echo "</ul>";
-
-		
-
-		echo "<div class='clear'>&nbsp;</div>";
-	echo "</div>";
-}
 include_once('footer.php');
 ?>
